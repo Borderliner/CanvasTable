f9f2619b113e894fe4a93dd0af90da48c8b6d50f
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-11-22 11:53:39 +0330

    add: custom styling

--- src/style/style.ts
+++ src/style/style.ts
@@ -4,15 +4,15 @@ type ITableStyleProps = ICanvasTable.ITableStyleProps
 export const DEFAULT_STYLE: ITableStyleProps = {
   width: '100%',
   height: '100%',
-  rowHeight: 55,
+  rowHeight: 36,
   columnWidth: 150,
   borderColor: '#e8e8e8',
   textColor: 'rgba(0,0,0,0.65)',
-  fontSize: '14px',
+  fontSize: '13px',
   fontFamily:
-    '-apple-system,BlinkMacSystemFont,"Segoe UI","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+    'Inter, -apple-system,BlinkMacSystemFont,"Segoe UI","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
   headerBackColor: '#fafafa',
-  headerRowHeight: 55,
+  headerRowHeight: 36,
   padding: 16,
 }
 
